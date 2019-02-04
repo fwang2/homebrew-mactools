d9a0bdc8da5679417a4df0ad3fe7ace7a7ceeadc
Feiyi Wang <fwang2@gmail.com>
2019-02-04 15:27:15 -0500

    fix

--- haba.rb
+++ haba.rb
@@ -1,4 +1,4 @@
-class haba < Formula
+class Haba < Formula
   desc "something about haba"
   homepage "https://github.com/fwang2/haba4mac"
   url="https://github.com/fwang2/haba4mac/archive/0.1.tar.gz"
