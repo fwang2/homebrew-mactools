9dd444c9b1ab4cd3febfa4757827a634e7e22834
Feiyi Wang <fwang2@gmail.com>
2019-02-04 15:36:52 -0500

    fix

--- haba.rb
+++ haba.rb
@@ -4,7 +4,7 @@ class Haba < Formula
   url "https://github.com/fwang2/haba4mac/archive/0.1.tar.gz"
   sha256 "49116092c27b734fac3a756888dab40162301d8950de3952a4ba81033df43fbb"
   
-  depends_on opencv => "4.0.1"
+  depends_on "opencv" => "4.0.1"
 
   def install
     bin.install "src/cli" => "cli"
