83b8374842f19386feae8248cc839c35feb6bc53
Feiyi Wang <fwang2@gmail.com>
2019-02-04 15:30:22 -0500

    add sha

--- haba.rb
+++ haba.rb
@@ -2,6 +2,7 @@ class Haba < Formula
   desc "something about haba"
   homepage "https://github.com/fwang2/haba4mac"
   url "https://github.com/fwang2/haba4mac/archive/0.1.tar.gz"
+  sha256 "49116092c27b734fac3a756888dab40162301d8950de3952a4ba81033df43fbb"
   def install
     bin.install "src/cli" => "habacli"
   end
