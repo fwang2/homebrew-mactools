84da768d80fa6a6a3c4dd3e324110b2097e44c53
Feiyi Wang <fwang2@gmail.com>
2019-02-04 16:34:39 -0500

    update

--- haba.rb
+++ haba.rb
@@ -2,7 +2,8 @@ class Haba < Formula
   desc "something about haba"
   homepage "https://github.com/fwang2/haba4mac"
   url "https://github.com/fwang2/haba4mac/archive/0.2.tar.gz"
-  
+  sha256 "48f7eae8c9cd4cbb96b7e548b9c4278c4b568a386e73febaa18ad595ca822ebf"
+
   depends_on "opencv" => "4.0.0"
 
    def haba_envs
@@ -15,7 +16,16 @@ class Haba < Formula
   def install
     bin.install "src/cli" => "cli"
     prefix.install "models" 
-    envs.write(haba_envs) unless envs.exist?
   end
+
+  def caveats; <<~EOS
+
+    Please set up the following environment variable:
+
+        export HABA_HOME="#{prefix}"
+
+  EOS
+  end
+
 end
 
