55ca59095caa6bbfcc3b3bef5ed9b524a0b24121
Feiyi Wang <fwang2@gmail.com>
2019-02-04 15:28:02 -0500

    fix url

--- haba.rb
+++ haba.rb
@@ -1,7 +1,7 @@
 class Haba < Formula
   desc "something about haba"
   homepage "https://github.com/fwang2/haba4mac"
-  url="https://github.com/fwang2/haba4mac/archive/0.1.tar.gz"
+  url "https://github.com/fwang2/haba4mac/archive/0.1.tar.gz"
   def install
     bin.install "src/cli" => "habacli"
   end
