ef93044a18dd584669d6c76c42e03dc76c332334
Feiyi Wang <fwang2@gmail.com>
2019-02-04 15:35:05 -0500

    add deps

--- haba.rb
+++ haba.rb
@@ -3,6 +3,9 @@ class Haba < Formula
   homepage "https://github.com/fwang2/haba4mac"
   url "https://github.com/fwang2/haba4mac/archive/0.1.tar.gz"
   sha256 "49116092c27b734fac3a756888dab40162301d8950de3952a4ba81033df43fbb"
+  
+  depends_on opencv => "4.0.1"
+
   def install
     bin.install "src/cli" => "habacli"
   end
