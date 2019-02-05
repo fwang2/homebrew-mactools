bae5dbbdc3847925bc293c006ea871e08482b5c7
Feiyi Wang <fwang2@gmail.com>
2019-02-04 23:56:18 -0500

    update to 0.3

--- haba.rb
+++ haba.rb
@@ -1,8 +1,8 @@
 class Haba < Formula
   desc "something about haba"
   homepage "https://github.com/fwang2/haba4mac"
-  url "https://github.com/fwang2/haba4mac/archive/0.2.tar.gz"
-  sha256 "48f7eae8c9cd4cbb96b7e548b9c4278c4b568a386e73febaa18ad595ca822ebf"
+  url "https://github.com/fwang2/haba4mac/archive/0.3.tar.gz"
+  sha256 "b66460f0a04d5ecf9e4e79edd951fcb9568358213da95e351d38b755604cb3c7"
 
   depends_on "opencv" => "4.0.0"
 
