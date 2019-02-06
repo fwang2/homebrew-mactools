df06a738a1449b7a54ef24e75630d42cd2e91068
Feiyi Wang <fwang2@gmail.com>
2019-02-06 12:23:09 -0500

    update shasum

--- hba.rb
+++ hba.rb
@@ -2,7 +2,7 @@ class Hba < Formula
   desc "Demo: A platform of human behavial analysis"
   homepage "https://github.com/fwang2/hba4mac"
   url "https://github.com/fwang2/haba4mac/archive/0.3.tar.gz"
-  sha256 "b66460f0a04d5ecf9e4e79edd951fcb9568358213da95e351d38b755604cb3c7"
+  sha256 "66c15c9e573cf923967a4fd09b749e3fef332e37bc2b70aafbfa2776f3091578"
 
   depends_on "opencv" => "4.0.0"
 
