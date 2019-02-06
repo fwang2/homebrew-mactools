7be91065ec22e5c75ba9347048a5d27b99b842ca
Feiyi Wang <fwang2@gmail.com>
2019-02-06 12:14:12 -0500

    change name

--- hba.rb
+++ hba.rb
@@ -1,14 +1,14 @@
-class Haba < Formula
+class Hba < Formula
   desc "something about haba"
-  homepage "https://github.com/fwang2/haba4mac"
+  homepage "https://github.com/fwang2/hba4mac"
   url "https://github.com/fwang2/haba4mac/archive/0.2.tar.gz"
   sha256 "48f7eae8c9cd4cbb96b7e548b9c4278c4b568a386e73febaa18ad595ca822ebf"
 
   depends_on "opencv" => "4.0.0"
 
-   def haba_envs
+   def hba_envs
     <<~EOS
-      export HABA_HOME="#{prefix}"
+      export HBA_HOME="#{prefix}"
     EOS
    end
 
@@ -22,7 +22,7 @@ class Haba < Formula
 
     Please set up the following environment variable:
 
-        export HABA_HOME="#{prefix}"
+        export HBA_HOME="#{prefix}"
 
   EOS
   end
