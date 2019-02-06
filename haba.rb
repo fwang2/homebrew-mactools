fc77e2fbacb47db8277d94c1be800a49faf5b1e9
Feiyi Wang <fwang2@gmail.com>
2019-02-05 20:15:42 -0500

    update desc

--- haba.rb
+++ haba.rb
@@ -1,5 +1,5 @@
 class Haba < Formula
-  desc "something about haba"
+  desc "Demo: A platform of human behavial analysis"
   homepage "https://github.com/fwang2/haba4mac"
   url "https://github.com/fwang2/haba4mac/archive/0.3.tar.gz"
   sha256 "b66460f0a04d5ecf9e4e79edd951fcb9568358213da95e351d38b755604cb3c7"
