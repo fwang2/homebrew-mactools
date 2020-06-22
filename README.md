c23ab49a607991cc24a21ab65b9def60323b53fa
Feiyi Wang <fwang2@gmail.com>
2020-06-22 09:34:56 -0400

    wip: update to 3.2.1

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
-# Mac tools
+# tools
+
+## Naming:
+
+According to [brew
+tap](https://github.com/Homebrew/brew/blob/master/docs/Taps.md), this repo is
+named as `fwang2/homebrew-tools` on github.
 
 
-to tap this repo:
+To tap this repo:
 
-    brew tap fwang2/mactools
+    brew tap fwang2/tools
 
+homebrew clone the github repo into
+`/usr/local/Homebrew/Library/Taps/fwang2/homebrew-tools`. For external users,
+it doesn't matter. For local developer, I make a sym link to make it easier to
+access.
 
 generate sha256 checksum
 
--- darshan-util.rb
+++ darshan-util.rb
@@ -1,12 +1,14 @@
 class DarshanUtil < Formula
     desc "Darshan: HPC I/O Characterization Tool"
     homepage "https://www.mcs.anl.gov/research/projects/darshan/"
-    url "https://github.com/fwang2/darshan/archive/v3-beta1.tar.gz"
-    sha256 "09d903bacd89ccc0c83061debfd9c0566cdf92dd7507728a91fc5600d94399c9"
+    #url "https://github.com/fwang2/darshan/archive/v3-beta1.tar.gz"
+    #sha256 "09d903bacd89ccc0c83061debfd9c0566cdf92dd7507728a91fc5600d94399c9"
+    url "https://github.com/fwang2/darshan-releases/raw/master/darshan-3.2.1.tar.gz"
+    sha256 "d63048b7a3d1c4de939875943e3e7a2468a9034fcb68585edbc87f57f622e7f7"
 
     def install
         Dir.chdir("darshan-util")
-        system "./configure", "--prefix=#{prefix}"
+        system "CFLAGS=\"-std=c99\" ./configure", "--prefix=#{prefix}"
         system "make", "install"
     end
 end
